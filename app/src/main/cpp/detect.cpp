fb5e255f49eb660074d8eb9ff5f59ed45a996976
qtfreet00 <812298893@qq.com>
2019-09-27 15:34:44 +0800

    add more check

--- app/src/main/cpp/detect.cpp
+++ app/src/main/cpp/detect.cpp
@@ -7,6 +7,8 @@
 #include <android/log.h>
 #include <pthread.h>
 #include <cstdlib>
+#include <elf.h>
+#include <link.h>
 #include <fcntl.h>
 
 extern "C" int wrap_openat(int, const char *, int, ...);
@@ -17,15 +19,19 @@ extern "C" int wrap_close(int __fd);
 
 extern "C" int wrap_kill(pid_t, int);
 
+#define BUFFER_LEN 512
+
 #define TAG "carleen"
 
 #define DEBUG
 
-#define BUFFER_LEN 512
-
-
+#ifdef DEBUG
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-
+#define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
+#else
+#define LOGW(...) ((void)0)
+#define LOGI(...) ((void)0)
+#endif
 
 int
 wrap_memcmp(const unsigned char *s1, const unsigned char *s2, size_t n) {
@@ -59,45 +65,6 @@ int find_mem_string(long long base, long long end, unsigned char *ptr, unsigned 
     return 0;
 }
 
-int scan_executes(char *buffer) {
-    char mode[10];
-    char path[256];
-    long long base;
-    long long end;
-    // "frida:rpc"
-    unsigned char s[] =
-            {
-
-                    0xfe, 0xba, 0xfb, 0x4a, 0x9a, 0xca, 0x7f, 0xfb,
-                    0xdb, 0xea, 0xfe, 0xdc
-            };
-
-    for (unsigned char &m : s) {
-        unsigned char c = m;
-        c = ~c;
-        c ^= 0xb1;
-        c = (c >> 0x6) | (c << 0x2);
-        c ^= 0x4a;
-        c = (c >> 0x6) | (c << 0x2);
-        m = c;
-    }
-
-    unsigned int length = 11;
-
-    if (sscanf(buffer, "%llx-%llx %s %*s %*s %*s %s", &base, &end, mode, path) != 4) {
-        return 0;
-    }
-
-    if (wrap_memcmp((unsigned char *) mode, (unsigned char *) "r-xp", 4) == 0 && path[0] != '[' &&
-        end - base > 1000000) {
-
-        return (find_mem_string(base, end, s, length) == 1);
-
-    } else {
-        return 0;
-    }
-}
-
 int read_line(int fd, char *ptr, unsigned int maxlen) {
     int n;
     int rc;
@@ -120,31 +87,87 @@ int read_line(int fd, char *ptr, unsigned int maxlen) {
     return (n);
 }
 
+int elf_check_header(uintptr_t base_addr) {
+    ElfW(Ehdr) *ehdr = (ElfW(Ehdr) *) base_addr;
+    if (0 != memcmp(ehdr->e_ident, ELFMAG, SELFMAG)) return 0;
+#if defined(__LP64__)
+    if(ELFCLASS64 != ehdr->e_ident[EI_CLASS]) return 0;
+#else
+    if (ELFCLASS32 != ehdr->e_ident[EI_CLASS]) return 0;
+#endif
+    if (ELFDATA2LSB != ehdr->e_ident[EI_DATA]) return 0;
+    if (EV_CURRENT != ehdr->e_ident[EI_VERSION]) return 0;
+    if (ET_EXEC != ehdr->e_type && ET_DYN != ehdr->e_type) return 0;
+    if (EV_CURRENT != ehdr->e_version) return 0;
+    return 1;
+}
+
+int wrap_endsWith(const char *str, const char *suffix) {
+    if (!str || !suffix)
+        return 0;
+    size_t lenA = strlen(str);
+    size_t lenB = strlen(suffix);
+    if (lenB > lenA)
+        return 0;
+    return strncmp(str + lenA - lenB, suffix, lenB) == 0;
+}
+
+
 void *check_loop(void *) {
     int fd;
+    char path[256];
+    char perm[5];
+    unsigned long offset;
+    unsigned int base;
+    long end;
     char buffer[BUFFER_LEN];
     int loop = 0;
-#ifdef DEBUG
+    unsigned int length = 11;
+    //"frida:rpc"
+    unsigned char frida_rpc[] =
+            {
+
+                    0xfe, 0xba, 0xfb, 0x4a, 0x9a, 0xca, 0x7f, 0xfb,
+                    0xdb, 0xea, 0xfe, 0xdc
+            };
+
+    for (unsigned char &m : frida_rpc) {
+        unsigned char c = m;
+        c = ~c;
+        c ^= 0xb1;
+        c = (c >> 0x6) | (c << 0x2);
+        c ^= 0x4a;
+        c = (c >> 0x6) | (c << 0x2);
+        m = c;
+    }
     LOGI("start check frida loop");
-#endif
     while (loop < 10) {
         fd = wrap_openat(AT_FDCWD, "/proc/self/maps", O_RDONLY, 0);
         if (fd > 0) {
             while ((read_line(fd, buffer, BUFFER_LEN)) > 0) {
 
-                if (scan_executes(buffer) == 1) {
-#ifdef DEBUG
+                if (sscanf(buffer, "%x-%lx %4s %lx %*s %*s %s", &base, &end, perm, &offset, path) !=
+                    5) {
+                    continue;
+                }
+                if (perm[0] != 'r') continue;
+                if (perm[3] != 'p') continue; //do not touch the shared memory
+                if (0 != offset) continue;
+                if (strlen(path) == 0) continue;
+                if ('[' == path[0]) continue;
+                if (end - base <= 1000000) continue;
+                if (wrap_endsWith(path, ".oat")) continue;
+                if (elf_check_header(base) != 1) continue;
+                if (find_mem_string(base, end, frida_rpc, length) == 1) {
                     LOGI("frida found in memory!");
-#else
+#ifndef DEBUG
                     wrap_kill(wrap_getpid(),SIGKILL);
 #endif
                     break;
                 }
             }
         } else {
-#ifdef DEBUG
             LOGI("open maps error");
-#endif
         }
         wrap_close(fd);
         loop++;
